0fd7c0bb3c02b1f7a9267db9b9681e42c56ca3b7
Philip Ingrey <philip.ingrey@googlemail.com>
2012-03-22 08:04:10 +0000

    improved second image and started on demo

--- create.js
+++ create.js
@@ -2,7 +2,8 @@ var Canvas = require('canvas')
   , Image = Canvas.Image
   , fs = require('fs')
   , counter = 0
-  , startingscr = process.argv[2];
+  , startingscr = process.argv[2]
+  , name = __dirname + '/' + startingscr.split('.')[0];
 
 
 function downscale(filenamein, filenameout, resdrop, cb){
@@ -18,7 +19,7 @@ function downscale(filenamein, filenameout, resdrop, cb){
 
     ctx.drawImage(img, 0, 0, w, h);
 
-    var out = fs.createWriteStream(__dirname + '/' + filenameout);
+    var out = fs.createWriteStream(filenameout);
 
     var stream = canvas.createJPEGStream({
       bufsize : 2048,
@@ -32,7 +33,7 @@ function downscale(filenamein, filenameout, resdrop, cb){
     stream.pipe(out);
   };
 
-  img.src = __dirname + '/' + filenamein;
+  img.src = filenamein;
 
 }
 
@@ -63,13 +64,13 @@ function createDiffImg(imgsrc1, imgsrc2, outsrc, cb){
       // make the diff, leveled at 127 and mod 256 (ignoring alpha channel)
       for(var i=0;i<img1.width*img1.height;i++){
           for(var j=0;j<3;j++){
-              data1.data[4*i+j] = Math.min(255,Math.max(0,2*(data1.data[4*i+j]-data2.data[4*i+j])+127)); 
+              data1.data[4*i+j] = Math.min(255,Math.max(0,(data1.data[4*i+j]-data2.data[4*i+j])+127)); 
           }
       }
 
       ctx.putImageData(data1,0,0);
 
-      var out = fs.createWriteStream(__dirname + '/' + outsrc);
+      var out = fs.createWriteStream(outsrc);
 
       var stream = canvas.createJPEGStream({
         bufsize : 2048,
@@ -84,10 +85,45 @@ function createDiffImg(imgsrc1, imgsrc2, outsrc, cb){
 
     }
 
-    img2.src = __dirname +'/' + imgsrc2;
+    img2.src = imgsrc2;
   }
 
-  img1.src = __dirname + '/' + imgsrc1;
+  img1.src = imgsrc1;
+}
+
+function testDecode(imgsrc1, imgsrc2, cb){
+  var img1 = new Image()
+    , img2 = new Image();
+
+  img1.onload = function(){
+    img2.onload = function(){
+        var w = img2.width
+          , h = img2.height
+          , canvas = new Canvas(w,h)
+          , ctx = canvas.getContext('2d');
+
+        ctx.drawImage(img1,0,0,w,h);
+        data1 = ctx.getImageData(0,0,w,h);
+        ctx.drawImage(img2,0,0,w,h);
+        data2 = ctx.getImageData(0,0,w,h);
+
+        // Use the diff
+
+        for(var i=0;i<img2.width*img2.height;i++){
+            for(var j=0;j<3;j++){
+                data1.data[4*i+j] = Math.min(255,Math.max(0,(data1.data[4*i+j]+((data2.data[4*i+j]-127) || 0))));
+            }
+        }
+
+        ctx.putImageData(data1,0,0);
+
+        if(cb) {
+          cb(ctx.canvas.toDataURL());
+        }
+    };
+    img2.src = imgsrc2;
+  };
+  img1.src = imgsrc1;
 }
 
 counter = 0;
@@ -95,21 +131,14 @@ function phaseTwo(){
   counter++;
   if(counter==2) {
     // images made - make the diffs
-    var name = startingscr.split('.')[0];
-    createDiffImg(name+'_med.jpg',name+'_low.jpg',name+'_low_to_med_diff.jpg');
-    createDiffImg(startingscr,name+'_med.jpg',name+'_med_to_full_diff.jpg');
+    createDiffImg(name+'_med.jpg',name+'_low.jpg',name+'_low_to_med_diff.jpg', function(){
+      testDecode(name+'_low.jpg',name+'_low_to_med_diff.jpg',function(medsrc){
+        createDiffImg(__dirname + '/' + startingscr,medsrc,name+'_med_to_full_diff.jpg');
+      });
+    });
   }
 }
 
 // make the images
-downscale(startingscr, startingscr.split('.')[0]+'_med.jpg', 2, phaseTwo);
-downscale(startingscr, startingscr.split('.')[0]+'_low.jpg', 4, phaseTwo);
-
-/*
-
-    console.log(++counter);
-    if(counter == resolution){
-      createDiffImg('images/test_1.jpg','images/test_2.jpg','images/diff_test.jpg',function(){console.log('should be done!');});
-    }
-  });
-  */
+downscale(__dirname + '/' + startingscr, name+'_low.jpg', 4, phaseTwo);
+downscale(__dirname + '/' + startingscr, name+'_med.jpg', 2, phaseTwo);
